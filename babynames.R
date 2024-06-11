094be53de80527ff8faa261ea456969f07db1c95
Jawaria Jaleel <jawariajaleel@Jawarias-MacBook-Pro.local>
2024-06-11 15:36:22 -0500

    visual

--- babynames.R
+++ babynames.R
@@ -1,6 +1,7 @@
 library(babynames)
 library(ggplot2)
 library(dplyr)
+library(tidyverse)
 bb_names <- babynames
 
 ggplot(data = bb_names %>% filter( name == "Mary", year >= "1982"), aes( x = year)) + geom_bar()
@@ -20,9 +21,22 @@ ggplot(data = women, aes( x = name, y = n))  + geom_col(alpha = 0.5, linewidth=2
 
 # 15) Create a new data set called the_nineties that only contains years from the 1990s.
 # 
-the_nineties <- bb_names %>%
+the_ninties <- bb_names %>%
   filter( year >= "1990")
 # 16) Save this dataset to your repository (use write.csv()).
 # 
-
+write_csv(the_ninties, "the_ninties.csv")
 # 17) Add, commit, and push your files to GitHub.
+
+# 18) In RStudio pull from GitHub. Is it already up to date?
+#   
+#   19) Now that everything is up to date, make a visualisation of you and your team member’s names for a year of your choice.
+# 
+team_names <- bb_names %>% filter( name %in% c("Mason", "Lucy", "Cheyenne", "Ruth") & year == '2004')
+
+ggplot(data = team_names, aes( x = name, y = n))+
+  geom_col() +
+  facet_wrap(~sex)
+# 20) Make a visual that looks at your name over time. What happens if you color by sex?
+#   
+#   21) Don’t forget to stage/add, commit, and push your hardwork to GitHub!
